e094e7ad650c3859b9b6b37fffffacd4dfe49158
Zloysvin <danylomartakov@gmil.com>
2021-09-06 10:17:58 +0300

    init commit

---
+++ testtestrep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testtestrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
